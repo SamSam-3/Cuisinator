c06582f956f4ea17b53e0009bd9dca5881258259
Xain <samuel.castre82@gmail.com>
2021-03-27 16:35:39 +0100

    Interaction bouton

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,39 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML private Button btnTest = new Button();
+
+        @FXML
+        public void handleButtonClick(ActionEvent evt){
+            Button test = (Button) evt.getSource();
+
+            switch(test.getId()){
+                case "categorie":
+                    //Faire le layer à gauche
+
+
+                case "course":
+                    //Faire le layer liste de course
+
+
+                case "avancée":
+                    //Faire le layer avancée
+
+            }
+
+
+            System.out.println(test.getId());
+        }
+
     public Controller() {
 
 
     }
+
 }
--- src/sample/View.java
+++ src/sample/View.java
@@ -1,14 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class View {
 
-    public View(){
-
-
-    }
-
-    public String toString(){
-
-        return "non";
-    }
 }
