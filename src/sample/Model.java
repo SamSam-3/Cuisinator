203f5dfafd9dc6a7b77c2813ae02b8b0c4f90e80
Me-k_01 <mecaman01@gmail.com>
2021-04-16 16:50:17 +0200

    Second algo de recherche

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -26,24 +26,48 @@ public class Model {
             throw new RuntimeException("Data base not found");
         }
     }
+    public Set<Recipe> search(String searchStr, Set<String> categsFilter, Set<String> ingredsFilter) {
+        if (categsFilter != null) {    
+            categsFilter = this.categories; // de base on recherche sur toutes les categories
+        }
+        Set<Recipe> output = new HashSet<Recipe>();
 
+        if (ingredsFilter != null) {
+            for (String cat : categsFilter) {
+                for (String ing : ingredsFilter) {
+                    for (Recipe recipe : this.recipeMap.get(cat).get(ing)) {
+                        String name = recipe.getName();
+                        if (name.toLowerCase().contains(searchStr)) {
+                            output.add(recipe);
+                        }
+                    }
+                }
+            }
+        } else {
+            for (Recipe recipe : this.recipeList) {
+                String name = recipe.getName();
+                if (name.toLowerCase().contains(searchStr)) {
+                    output.add(recipe);
+                }
+            }
+        }
+
+        return output;
+    }
 
     public void search(String input) {
-
-
         //////// Recettes \\\\\\\\
         System.out.println("############################################# Recettes #####################################################");
+
         for (Recipe recipe : recipeList) {
             String name = recipe.getName();
             if (name.toLowerCase().contains(input)) { // A modifier pour faire des recherches sans accents et autres caracteres spéciaux
-
                 this.control.addRecipe(name);
                 this.control.recipeDisplay.add(recipe);
 
                 System.out.println(name); // Affiche les recettes correspondantes
             }
         }
-
 
         //////// Ingrédients \\\\\\\\
         System.out.println("############################################# Ingrédients #################################################");
