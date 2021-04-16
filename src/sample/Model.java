a6e55a5a9c82619c823aefa24a1ad590a8623a58
Me-k_01 <mecaman01@gmail.com>
2021-04-16 11:30:24 +0200

    Suppresion de la class view

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -13,19 +13,12 @@ public class Model {
     public RecipeMap recipeMap;
     public ArrayList<Recipe> recipeList;
 
-
-    private View view;
     private Controller control;
     private Set<String> categories;
-    private ArrayList<String> ingredsLeft;
-    private ArrayList<Recipe> recipeDisplay;
 
-    public Model(View view, Controller control) {
-        this.view = view;
+    public Model(Controller control) {
         this.control = control;
         this.loadData();
-        this.ingredsLeft = new ArrayList<String>();
-        this.recipeDisplay = new ArrayList<Recipe>();
         this.categories = recipeMap.getCategories();
     }
 
@@ -40,46 +33,13 @@ public class Model {
         }
     }
 
-    public void showLayer(String btn){
-
-        // Slide layer categorie vers la droite
-        // Slide layer liste de course vers la gauche
-        // Apparition layer recherche avancée
-
-        switch (btn) { // TODO: a modifier
-            case "categorie" -> {
-                for (String name : this.categories) {
-                    this.view.newCategories(name); 
-                }
-                this.view.showCategories();
-            }
-            case "course" -> {
-                for (String s : this.ingredsLeft) {
-                    this.view.newIngredient(s);
-                }
-                this.view.showIngredients();
-            }
-            case "avancee" -> this.view.showAdvanced();
-        }
-
-    }
-
-    public void actualRecipe(String recipeName){
-
-        for (Recipe recipe : this.recipeDisplay){
-            if (recipe.getName().equals(recipeName)) {
-                this.view.showRecipe(recipe, this.ingredsLeft);
-            }
-        }
-
-    }
 
     public void search(String input) {
 
         if(input.length()>0){
 
-            this.recipeDisplay.clear();
-            this.view.wipe(input);
+            this.control.recipeDisplay.clear();
+            this.control.wipe(input);
 
             //////// Recettes \\\\\\\\
             System.out.println("############################################# Recettes #####################################################");
@@ -87,8 +47,8 @@ public class Model {
                 String name = recipe.getName();
                 if (name.toLowerCase().contains(input)) { // A modifier pour faire des recherches sans accents et autres caracteres spéciaux
 
-                    this.view.addRecipe(name);
-                    this.recipeDisplay.add(recipe);
+                    this.control.addRecipe(name);
+                    this.control.recipeDisplay.add(recipe);
                     System.out.println(name); // Affiche les recettes correspondantes
                 }
             }
@@ -99,19 +59,23 @@ public class Model {
 
             for(String ing : recipeMap.getIngredients()){
                 if(ing.toLowerCase().contains(input)){
-                    this.view.addIngredients(ing);
+                    this.control.addIngredients(ing);
                     System.out.println(ing);
                 }
             }
 
             System.out.println("############################################################################################################");
 
-            this.view.saveState();
+            this.control.saveState();
         }
 
         if (input.length() == 0) {
-            this.recipeDisplay.clear();
-            this.view.wipe(input);
+            this.control.recipeDisplay.clear();
+            this.control.wipe(input);
         }
+    }
+
+    public Set<String> getCategories() {
+        return categories;
     }
 }
